675ab2e4c90933d97c3ad78e9611de59ee9b6036
Nicothekiller <nicolasnaran@gmail.com>
2024-11-05 12:02:23 -0500

    added multithreading
    added multithreading when compiling .o files

--- flags.go
+++ flags.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 // struct for flags to compile
@@ -26,25 +27,32 @@ func newCompileFlags() CompileFlags {
 // func for compiling with the given flags
 func (flags *CompileFlags) compile() {
 	files := getFiles()
+	var wg sync.WaitGroup
 
 	for _, v := range files {
-		ndFile := strings.Split(v, "/")
+		wg.Add(1)
+		go func() {
+			ndFile := strings.Split(v, "/")
 
-		oFile := "bin/" + ndFile[len(ndFile)-1] + ".o"
-		fFlags := []string{"-c", v, "-o", oFile}
+			oFile := "bin/" + ndFile[len(ndFile)-1] + ".o"
+			fFlags := []string{"-c", v, "-o", oFile}
 
-		fFlags = append(fFlags, flags.compFlags...)
+			fFlags = append(fFlags, flags.compFlags...)
 
-		compComand := exec.Command(flags.compiler, fFlags...)
-		compComand.Stdout = os.Stdout
-		compComand.Stderr = os.Stderr
+			compComand := exec.Command(flags.compiler, fFlags...)
+			compComand.Stdout = os.Stdout
+			compComand.Stderr = os.Stderr
 
-		fmt.Print(flags.compiler, " ")
-		fmt.Println(fFlags)
+			fmt.Println(flags.compiler, fFlags)
 
-		err := compComand.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
+			err := compComand.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer wg.Done()
+		}()
 	}
+
+	wg.Wait()
 }
